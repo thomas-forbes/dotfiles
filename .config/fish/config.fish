56093b8a058ead5aa7b24b8e50eae8a036093aab
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2026-03-07 15:46:43 -0500

    open source

---
+++ .config/fish/config.fish
@@ -0,0 +1,48 @@
+set -U fish_greeting
+
+fish_add_path $HOME/.bun/bin
+fish_add_path $HOME/Library/pnpm
+
+set -x BREW_PREFIX "/opt/homebrew"
+eval "$($BREW_PREFIX/bin/brew shellenv)"
+
+function fish_prompt
+    set_color red
+    if git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
+        set -l branch (git branch --show-current 2>/dev/null)
+        if test -n "$branch"
+            set_color magenta
+            echo -n " [$branch] "
+        end
+    end
+
+    set_color cyan
+    echo -n (prompt_pwd)
+
+    if test $status -eq 0
+        set_color green
+        echo -n ' > '
+    else
+        set_color red
+        echo -n ' > '
+    end
+
+    set_color normal
+end
+
+function fish_right_prompt
+    set_color brblack
+    echo -n (date "+%H:%M:%S")
+    set_color normal
+end
+
+if status is-interactive
+    abbr -a c clear
+    abbr -a e exit
+    abbr -a l "ls -la"
+    abbr -a zedc "zed .; exit"
+    abbr -a lg lazygit
+    abbr -a gh-repo-push "gh repo create --source=. --private --push"
+
+    yadm status -s
+end
