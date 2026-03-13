7ea6fe67ea6b2c0dba5d3065d432111323dfa617
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2026-03-13 00:29:36 +0000

    fnm env vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,16 @@ fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/Library/pnpm
 fish_add_path $HOME/.cargo/bin
 
+set -gx PATH "/Users/thomasforbes/.local/state/fnm_multishells/1886_1773360806649/bin" $PATH;
+set -gx FNM_MULTISHELL_PATH "/Users/thomasforbes/.local/state/fnm_multishells/1886_1773360806649";
+set -gx FNM_VERSION_FILE_STRATEGY "local";
+set -gx FNM_DIR "/Users/thomasforbes/.local/share/fnm";
+set -gx FNM_LOGLEVEL "info";
+set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
+set -gx FNM_COREPACK_ENABLED "false";
+set -gx FNM_RESOLVE_ENGINES "true";
+set -gx FNM_ARCH "arm64";
+
 set -x BREW_PREFIX "/opt/homebrew"
 eval "$($BREW_PREFIX/bin/brew shellenv)"
 
