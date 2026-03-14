e5ade7e278fabf2abf297db3a489f0cc5afed4dc
thomas-forbes <34174315+thomas-forbes@users.noreply.github.com>
2026-03-14 15:41:53 +0000

    add git status indicator to fish prompt + rectangle changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,19 +26,25 @@ set -x BREW_PREFIX "/opt/homebrew"
 eval "$($BREW_PREFIX/bin/brew shellenv)"
 
 function fish_prompt
+    set -l last_status $status
+
     set_color red
     if git rev-parse --is-inside-work-tree >/dev/null 2>/dev/null
         set -l branch (git branch --show-current 2>/dev/null)
         if test -n "$branch"
             set_color magenta
             echo -n "[$branch] "
+            if test -n "$(git status --porcelain 2>/dev/null)"
+                set_color yellow
+                echo -n "• "
+            end
         end
     end
 
     set_color cyan
     echo -n (prompt_pwd)
 
-    if test $status -eq 0
+    if test $last_status -eq 0
         set_color green
         echo -n ' > '
     else
